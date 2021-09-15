2910d8797ac316c6643f0beaae4f9ba6d80ecc0d
Sudhansu Panda <sudhansupandaaa@gmail.com>
2021-09-16 04:35:25 +0530

    final

---
+++ app.js
@@ -0,0 +1,29 @@
+var txtinput = document.querySelector("#txtinput");
+var txtoutput = document.querySelector("#txtoutput");
+var btnTranslate = document.querySelector("#btn");
+
+const apiUrl = "https://api.funtranslations.com/translate/minion.json";
+
+btnTranslate.addEventListener("click", clickHandler);
+
+function getFinalApi(text) {
+    return apiUrl + "?" + "text=" + text;
+}
+
+function errorHandler(err) {
+    console.log("Theres an Error" + err);
+    alert("Something Wrong with the Server ! Please Try Again");
+}
+
+function clickHandler() {
+    var inputValue = txtinput.value;
+    
+    fetch(getFinalApi(inputValue))
+        .then((response) => response.json())
+        .then((json) => {
+            var minionsText = json.contents.translated;
+            console.log(minionsText);
+            txtoutput.innerText = minionsText;
+        })
+        .catch(errorHandler);
+}
